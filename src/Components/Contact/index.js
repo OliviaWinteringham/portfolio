391a1383091d307b28a563498f037a635c8a7205
Olivia Winteringham <owinteringham@gmail.com>
2019-04-26 16:40:01 +0100

    NavBar

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -6,12 +6,13 @@ const Contact = () => {
   return (
     <>
       <NavBar />
-      <div className={css.contactText}>
-        <h1> want to collaborate? fancy a chat? please get in touch></h1>
-        <p>
-          I'd like to hear from you email me{" "}
-          <strong>owinteringham at gmail.com</strong>{" "}
-        </p>
+      <div className={css.header}>
+        <div className={css.contactText}>
+          <h1> want to collaborate? fancy a chat? please get in touch</h1>
+          <p>
+            send me an email: <strong>owinteringham [at] gmail.com</strong>{" "}
+          </p>
+        </div>
       </div>
     </>
   );
