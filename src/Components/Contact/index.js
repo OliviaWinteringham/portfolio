d90e45ec5e124e2020c3054a1775dcdf23d00d4f
[Olivia Winteringham] <owinteringham@gmail.com>
2019-05-06 16:36:42 +0100

    twitter contact

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -9,8 +9,7 @@ const Contact = () => {
       <div className={css.header}>
         <div className={css.contactText}>
           <h1> want to collaborate? fancy a chat? please get in touch</h1>
-          <p>
-            send me an email: <strong>owinteringham [at] gmail.com</strong>{" "}
+          <p> get in touch via twitter <strong>@owinteringham</strong>{" "}
           </p>
         </div>
       </div>
