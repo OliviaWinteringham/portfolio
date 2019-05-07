87101f4455f0e9df9562a82074887e93ea9fb41d
[Olivia Winteringham] <owinteringham@gmail.com>
2019-05-07 13:49:43 +0100

    contact opens in new tab

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -12,7 +12,7 @@ const Contact = () => {
       <div className={css.header}>
         <div className={css.contactText}>
           <h1> want to collaborate? fancy a chat? please get in touch</h1>
-          <Link to="/twitter">
+          <Link to="/twitter" target="_blank">
           <h2> get in touch via twitter <strong>@owinteringham</strong>{" "}
           </h2>
       </Link>
