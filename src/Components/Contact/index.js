802f033e18fb40fff7ef1c8e7679fe835787a8c9
[Olivia Winteringham] <owinteringham@gmail.com>
2019-05-14 22:28:47 +0100

    twitter

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -15,8 +15,9 @@ const Contact = () => {
       <div className={css.header}>
         <div className={css.contactText}>
           <h1> want to collaborate? fancy a chat? please get in touch</h1>
-          <Link to="/twitter" target="_blank">
-          <h2> get in touch via twitter <strong>@owinteringham</strong>{" "}
+          {/* <Link to="/twitter" target="_blank"></Link> */}
+          <Link to="/twitter">
+          <h2> get in touch via twitter @owinteringham
           </h2>
       </Link>
         </div>
