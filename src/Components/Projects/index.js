05a4edda2bdde0f53f806e9a785b7dc29486d77b
[Olivia Winteringham] <owinteringham@gmail.com>
2019-06-12 14:20:52 +0100

    fixed image

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -12,7 +12,7 @@ const Projects = () => {
       <main>
       {/* <h1 className={css.projectText}>Projects... coming soon</h1> */}
       <div className={css.container}>
-            <img className={css.image} src={queen} alt='a beach' ></img>
+            <img className={css.image} src={queen} alt='queen of hearts'></img>
               <article className={css.overlay}>
                 <div className={css.overlayText}>'52 cards from around the world'<br></br> coming soon... </div>
               </article>
